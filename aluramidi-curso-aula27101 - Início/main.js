7dbf0d898a57493f4555978a8b1aeb7a39063d3d
joaquimsilvaneto <110394039+joaquimsilvaneto@users.noreply.github.com>
2023-10-27 08:43:41 -0300

    Add files via upload

---
+++ aluramidi-curso-aula27101 - Início/main.js
@@ -0,0 +1,29 @@
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+
+while(contador < listaDeTeclas.length) {
+     const tecla = listaDeTeclas[contador]
+     const instrumento = listaDeTeclas[contador].classList[1];
+     console.log (instrumento);
+     const  idAudio =  `#som_${instrumento}`    
+      tecla.onclick = function() {
+       tocaSom(`#som_tecla_pom`);
+
+
+      }
+
+    
+
+  
+
+    contador = contador + 1;
+
+
+    console.log(contador);
+  }
